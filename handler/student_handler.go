8b306805e84f162f64c36ebf0100b47e6931330e
Bayu Firmansyah <bayufirmansyah203@gmail.com>
2025-11-24 18:16:07 +0700

    add: student handler

--- handler/student_handler.go
+++ handler/student_handler.go
@@ -1,1 +1,27 @@
 package handler
+
+import (
+	"fmt"
+
+	"github.com/bayuf/Go-CRUD-Mahasiswa/dto"
+	"github.com/bayuf/Go-CRUD-Mahasiswa/services"
+)
+
+type StudentHandler struct {
+	service services.StudentService
+}
+
+func NewStudentHandler(service services.StudentService) StudentHandler {
+	return StudentHandler{
+		service: service,
+	}
+}
+
+func (h StudentHandler) Create(req dto.CreateStudentRequest) {
+	if err := h.service.Create(req); err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+
+	fmt.Println("Student added")
+}
